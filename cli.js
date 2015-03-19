7631fbf058188cbf0aa1870fe011ac6925f6f320
Devin Weaver <suki@tritarget.org>
2015-03-19 16:50:22 -0400

    Add a JsonWatcher
    This watcher will output an object usable by JSON.stringify().

--- cli.js
+++ cli.js
@@ -5,16 +5,23 @@ var projectDir, watcher;
 
 if (args.h || args.help) {
   console.log([
-    'Usage: git-watch-log [projectDir]',
+    'Usage: git-watch-log [--json] [projectDir]',
     '  projectDir defaults to current directory',
+    '  -j, --json  output in JSON format',
     'Press CTRL-C to exit.'
   ].join('\n'));
   process.exit(0);
 }
 
+var jsonMode = (args.j || args.json);
+
 projectDir = args._[0] || process.cwd();
 
-watcher = new GitWatcher.TerminalWatcher(projectDir);
+if (jsonMode) {
+  watcher = new GitWatcher.JsonWatcher(projectDir);
+} else {
+  watcher = new GitWatcher.TerminalWatcher(projectDir);
+}
 
 process.on('SIGINT', function() {
   watcher.close();
@@ -25,8 +32,20 @@ watcher.on('error', function(output) {
   console.error(output);
 });
 
-watcher
-  .on('refresh', function(output) {
-    console.log(output);
-  })
-  .watch();
+if (jsonMode) {
+  watcher
+    .on('refresh', function(output) {
+      console.log(JSON.stringify(output));
+      process.exit(0);
+    })
+    .on('error', function(output) {
+      process.exit(1);
+    })
+    .refresh();
+} else {
+  watcher
+    .on('refresh', function(output) {
+      console.log(output);
+    })
+    .watch();
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var GitWatcher = require('./lib/watcher');
 
 module.exports = {
   Watcher:             GitWatcher,
+  JsonWatcher:         require('./lib/json-watcher'),
   TerminalWatcher:     require('./lib/terminal-watcher'),
   NotImplementedError: GitWatcher.NotImplementedError
 };
