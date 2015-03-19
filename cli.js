30bb91e613f213a6fa99ac0068125ac4547e18ec
Devin Weaver <suki@tritarget.org>
2015-03-19 16:47:32 -0400

    Refactored code to multiple modules
    I realized that I wanted to do more then just terminal output. So I
    refactored the code into separate modules.
    
    The API has changed to offer a Watcher main class and a TerminalWatcher
    designed to do the same thing as before. In the future there will be
    more Watchers (like  JsonWatcher) that can be leveraged.

--- cli.js
+++ cli.js
@@ -1,12 +1,32 @@
 #!/usr/bin/env node
+var GitWatcher = require('./');
+var args       = require('optimist').argv;
+var projectDir, watcher;
 
-var gitLogWatch = require('./');
+if (args.h || args.help) {
+  console.log([
+    'Usage: git-watch-log [projectDir]',
+    '  projectDir defaults to current directory',
+    'Press CTRL-C to exit.'
+  ].join('\n'));
+  process.exit(0);
+}
 
-var projectPath = process.argv[2] || process.cwd();
+projectDir = args._[0] || process.cwd();
 
-var watcher = gitLogWatch(projectPath);
+watcher = new GitWatcher.TerminalWatcher(projectDir);
 
 process.on('SIGINT', function() {
   watcher.close();
   process.exit(0);
 });
+
+watcher.on('error', function(output) {
+  console.error(output);
+});
+
+watcher
+  .on('refresh', function(output) {
+    console.log(output);
+  })
+  .watch();
--- index.js
+++ index.js
@@ -1,56 +1,7 @@
-var chokidar = require('chokidar');
-var path     = require('path');
-var exec     = require('child_process').exec;
+var GitWatcher = require('./lib/watcher');
 
-var gitLog = [
-  'log', '--all', '--graph',
-  '--pretty=format:"%Cred%h%Creset',
-  '-%C(yellow)%d%Creset',
-  '%s %Cgreen(%cr)',
-  '%Cblue<%an>%Creset"',
-  '--abbrev-commit',
-  '--date=relative'
-].join(' ');
-
-var gitStatus = 'status -sb';
-
-var clear = function() {
-  console.log('\033c');
-};
-
-var debounce = function(fn, delay) {
-  var timer = null;
-  return function() {
-    var context = this, args = arguments;
-    clearTimeout(timer);
-    timer = setTimeout(function() {
-      fn.apply(context, args);
-    }, delay);
-  };
-};
-
-var execGit = function(command, cwd) {
-  return function() {
-    exec('git ' + command, {cwd: cwd}, function(err, stdout, stderr) {
-      if (err) {
-        console.error(stderr);
-        return;
-      }
-      console.log(stdout);
-    });
-  };
-};
-
-module.exports = function(projectPath) {
-  var glob   = path.resolve(path.join(projectPath, '.git/**'));
-  var status = execGit('status -sb', projectPath);
-  var logger = execGit(gitLog, projectPath);
-  var update = function() {
-    clear();
-    status();
-    logger();
-  };
-  update();
-  return chokidar.watch(glob, {ignoreInitial: true})
-    .on('all', debounce(update, 400));
+module.exports = {
+  Watcher:             GitWatcher,
+  TerminalWatcher:     require('./lib/terminal-watcher'),
+  NotImplementedError: GitWatcher.NotImplementedError
 };
